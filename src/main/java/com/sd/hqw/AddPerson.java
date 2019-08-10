5877c85a1bfe0566d66b87f1a252838053bced32
huangqiangwen <shangdahqw@163.com>
2019-08-10 12:38:23 +0800

    first commit

---
+++ src/main/java/com/sd/hqw/AddPerson.java
@@ -0,0 +1,80 @@
+package com.sd.hqw;
+
+import com.sd.hqw.message.AddressBookProtos;
+
+import java.io.*;
+
+/** Created by ginozhang on 2017/2/8. */
+public class AddPerson {
+
+  // 通过用户输入构建一个Person对象
+  static AddressBookProtos.Person promptForAddress(BufferedReader stdin, PrintStream stdout)
+      throws IOException {
+    AddressBookProtos.Person.Builder person = AddressBookProtos.Person.newBuilder();
+
+    stdout.print("Enter person ID: ");
+    person.setId(Integer.valueOf(stdin.readLine()));
+
+    stdout.print("Enter name: ");
+    person.setName(stdin.readLine());
+
+    stdout.print("Enter email address (blank for none): ");
+    String email = stdin.readLine();
+    if (email.length() > 0) {
+      person.setEmail(email);
+    }
+
+    while (true) {
+      stdout.print("Enter a phone number (or leave blank to finish): ");
+      String number = stdin.readLine();
+      if (number.length() == 0) {
+        break;
+      }
+
+      AddressBookProtos.Person.PhoneNumber.Builder phoneNumber =
+          AddressBookProtos.Person.PhoneNumber.newBuilder().setNumber(number);
+
+      stdout.print("Is this a mobile, home, or work phone? ");
+      String type = stdin.readLine();
+      if (type.equals("mobile")) {
+        phoneNumber.setType(AddressBookProtos.Person.PhoneType.MOBILE);
+      } else if (type.equals("home")) {
+        phoneNumber.setType(AddressBookProtos.Person.PhoneType.HOME);
+      } else if (type.equals("work")) {
+        phoneNumber.setType(AddressBookProtos.Person.PhoneType.WORK);
+      } else {
+        stdout.println("Unknown phone type.  Using default.");
+      }
+
+      person.addPhones(phoneNumber);
+    }
+
+    return person.build();
+  }
+
+  // 加载指定的序列化文件（如不存在则创建一个新的），再通过用户输入增加一个新的联系人到地址簿，最后序列化到文件中
+  public static void main(String[] args) throws Exception {
+    if (args.length != 1) {
+      System.err.println("Usage:  AddPerson ADDRESS_BOOK_FILE");
+      System.exit(-1);
+    }
+
+    AddressBookProtos.AddressBook.Builder addressBook = AddressBookProtos.AddressBook.newBuilder();
+
+    // Read the existing address book.
+    try {
+      addressBook.mergeFrom(new FileInputStream(args[0]));
+    } catch (FileNotFoundException e) {
+      System.out.println(args[0] + ": File not found.  Creating a new file.");
+    }
+
+    // Add an address.
+    addressBook.addPeople(
+        promptForAddress(new BufferedReader(new InputStreamReader(System.in)), System.out));
+
+    // Write the new address book back to disk.
+    FileOutputStream output = new FileOutputStream(args[0]);
+    addressBook.build().writeTo(output);
+    output.close();
+  }
+}
---
+++ src/main/java/com/sd/hqw/ListPeople.java
@@ -0,0 +1,49 @@
+package com.sd.hqw;
+
+import com.sd.hqw.message.AddressBookProtos;
+
+import java.io.FileInputStream;
+
+/** Created by ginozhang on 2017/2/8. */
+public class ListPeople {
+
+  // 打印地址簿中所有联系人信息
+  static void print(AddressBookProtos.AddressBook addressBook) {
+    for (AddressBookProtos.Person person : addressBook.getPeopleList()) {
+      System.out.println("Person ID: " + person.getId());
+      System.out.println("  Name: " + person.getName());
+      if (!person.getPhonesList().isEmpty()) {
+        System.out.println("  E-mail address: " + person.getEmail());
+      }
+
+      for (AddressBookProtos.Person.PhoneNumber phoneNumber : person.getPhonesList()) {
+        switch (phoneNumber.getType()) {
+          case MOBILE:
+            System.out.print("  Mobile phone #: ");
+            break;
+          case HOME:
+            System.out.print("  Home phone #: ");
+            break;
+          case WORK:
+            System.out.print("  Work phone #: ");
+            break;
+        }
+        System.out.println(phoneNumber.getNumber());
+      }
+    }
+  }
+
+  // 加载指定的序列化文件，并输出所有联系人信息
+  public static void main(String[] args) throws Exception {
+    if (args.length != 1) {
+      System.err.println("Usage:  ListPeople ADDRESS_BOOK_FILE");
+      System.exit(-1);
+    }
+
+    // Read the existing address book.
+    AddressBookProtos.AddressBook addressBook =
+        AddressBookProtos.AddressBook.parseFrom(new FileInputStream(args[0]));
+
+    print(addressBook);
+  }
+}
